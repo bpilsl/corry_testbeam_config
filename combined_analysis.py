1513f87787331649ba7d0f568281da79b01da963
ybuch <yannik.buch@web.de>
2022-07-19 18:15:13 +0200

    minor bugfix and added csv file since formatting is relevant and reproducing can be a pain. Also its <200kb

--- combined_analysis.py
+++ combined_analysis.py
@@ -14,7 +14,7 @@ def parse_args():
     add_arg('-n', help='number of events', type=int, default = 50000)
     add_arg('-c', '--comment', type=str, help='comment to output file name')
     add_arg('-f', '--framework', type=str, default='', help='framework to be used for analysis. corry or tbsw')
-    add_arg('--file', type=str, default='export.csv', help='path to csv file')
+    add_arg('--file', type=str, default='export_with_text.csv', help='path to csv file')
     add_arg('--col_start', type=str, default='0', help='start column ROI plotting')
     add_arg('--col_stop', type=str, default='512', help='stop column ROI plotting')
     add_arg('--row_start', type=str, default='0', help='start row ROI plotting')
@@ -51,10 +51,10 @@ if __name__ == '__main__':
 
     if args.framework.lower() == 'corry':
         if args.run:
-            os.system(f'analyze.py -r {args.run} -g {params[0][0]} -n {args.n}')
+            os.system(f'python3 analyze.py -r {args.run} -g {params[0][0]} -n {args.n}')
         elif args.start and args.stop:
             for i,run in enumerate(range(int(args.start), int(args.stop)+1)):
-                os.system(f'analyze.py -r {str(run)} -g {params[i][0]} -n {args.n}')
+                os.system(f'python3 analyze.py -r {str(run)} -g {params[i][0]} -n {args.n}')
     elif args.framework.lower() == 'tbsw':
         if args.run:
             os.system(f'bash {args.tbsw_workspace}scan_runs.sh {args.run} {args.run} gear{params[0][0]}.xml {caltag}')
